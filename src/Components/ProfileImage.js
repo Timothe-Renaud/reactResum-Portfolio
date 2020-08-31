af8ddb8b4813304ed05b032f08790711690c2041
timothé - renaud langlois - teston <timothe8833@gmail.com>
2020-08-31 11:32:54 +0200

    update contenue en général

--- src/Components/ProfileImage.js
+++ src/Components/ProfileImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ProfileImage extends React.Component {
   render() {
     return (<img 
-    src={"https://instagram.fcdg2-1.fna.fbcdn.net/v/t51.2885-15/e35/51794404_613747609071665_3929976282926976153_n.jpg?_nc_ht=instagram.fcdg2-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=NjsB6b-Nu-0AX-fIamR&oh=4201737e6c409374e767ad4d38c2b1f9&oe=5F6EDF41"} 
+    src={"https://i.imgur.com/t93JrJo.jpg"} 
     alt="Avatar"
     className="profileImage"/>);
   }
